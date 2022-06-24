a357b314cb4069fbf95b105bf2fa7b1d5066c318
Agentpoles <pauloams16@gmail.com>
2022-06-24 11:32:23 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState(0);
+  const [candidateId, setCandidateId] = useState(0);
+  const [votersAddres, setVotersAddress] = useState("");
+
+  //web2 functions
+
+  const handleAddCandidate = (e) => {};
+
+  const handleAuthorize = (e) => {};
+
+  const handleVote = (e) => {};
+
+  const handleGetCandidateCount = (e) => {
+    setResult(100);
+  };
+
+  const handleGetVotersWeight = (e) => {};
+
+  const handleChangeVotersAddress = (e) => {
+    setVotersAddress(e.target.value);
+  };
+
+  const handleChangeCandidateId = (e) => {
+    setCandidateId(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App-header">
+      <h1>{result}</h1>
+      <section>
+        <span>Add Candidate</span>
+        <br />
+        <input
+          type={"number"}
+          placeholder="candidateId"
+          onChange={(e) => handleChangeCandidateId(e)}
+        ></input>
+        <br></br>
+        <button
+          style={{ backgroundColor: "green", borderColor: "green" }}
+          onClick={(e) => handleAddCandidate(e)}
         >
-          Learn React
-        </a>
-      </header>
+          Add Candidate
+        </button>
+      </section>
+      <section style={{ marginTop: "30px" }}>
+        <span>Authorize</span>
+        <br />
+        <input
+          type={"string"}
+          placeholder="voter's address"
+          onChange={(e) => handleChangeVotersAddress(e)}
+        ></input>
+        <br></br>
+        <button
+          style={{ backgroundColor: "green", borderColor: "green" }}
+          onClick={(e) => handleAuthorize(e)}
+        >
+          Authorize
+        </button>
+      </section>
+
+      <section style={{ marginTop: "30px" }}>
+        <span>Vote</span>
+        <br />
+        <input
+          type={"number"}
+          placeholder="candidates id"
+          onChange={(e) => handleChangeCandidateId(e)}
+        ></input>
+        <br></br>
+        <button
+          style={{ backgroundColor: "green", borderColor: "green" }}
+          onClick={(e) => handleVote(e)}
+        >
+          vote
+        </button>
+      </section>
+
+      <section style={{ marginTop: "30px" }}>
+        <br></br>
+        <button
+          style={{ backgroundColor: "green", borderColor: "green" }}
+          onClick={(e) => handleGetVotersWeight(e)}
+        >
+          getVotersWeight;
+        </button>
+        <button
+          style={{
+            backgroundColor: "green",
+            borderColor: "green",
+            marginLeft: "30px",
+          }}
+          onClick={(e) => handleGetCandidateCount(e)}
+        >
+          getCandidateCount;
+        </button>
+      </section>
     </div>
   );
 }
